d18d586d1c8c65584dec755c0b2e1224526040da
Unity Technologies <@unity>
2018-01-25 00:00:00 +0000

    com.autodesk.fbx@2.0.0-preview.5 ## [2.0.0-preview.5] - 2018-01-25
    CHANGES
    * Moved tests into separate package

--- Runtime/Scripts/NativeMethods.cs
+++ Runtime/Scripts/NativeMethods.cs
@@ -223,14 +223,14 @@ class NativeMethods {
   /// When deploying with Unity Package Manager, do not add defines: the
   /// default platform defines suffice.
   /// </summary>
-#if COM_UNITY_FORMATS_FBX_AS_ASSET || UNITY_STANDALONE 
-  const string DllImportName = "UnityFbxSdkNative";
-#elif UNITY_EDITOR_OSX
-  const string DllImportName = "Packages/com.autodesk.fbx/MacOS/UnityFbxSdkNative.bundle/Contents/MacOS/UnityFbxSdkNative";
+#if UNITY_EDITOR_OSX
+  const string DllImportName = "Packages/com.autodesk.fbx/Runtime/Plugins/x86_64/UnityFbxSdkNative.bundle/Contents/MacOS/UnityFbxSdkNative";
 #elif UNITY_EDITOR_LINUX
-  const string DllImportName = "Packages/com.autodesk.fbx/Linux/UnityFbxSdkNative.so";
+  const string DllImportName = "Packages/com.autodesk.fbx/Runtime/Plugins/x86_64/UnityFbxSdkNative.so";
 #elif UNITY_EDITOR_WIN
-  const string DllImportName = "Packages/com.autodesk.fbx/Windows/UnityFbxSdkNative.dll";
+  const string DllImportName = "Packages/com.autodesk.fbx/Runtime/Plugins/x86_64/UnityFbxSdkNative.dll";
+#elif COM_UNITY_FORMATS_FBX_AS_ASSET || UNITY_STANDALONE 
+  const string DllImportName = "UnityFbxSdkNative";
 #else
   #error "FbxSdk: C# bindings for this platform haven't been implemented yet, sorry."
   const string DllImportName = "UnityFbxSdkNative";
